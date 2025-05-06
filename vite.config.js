dc9bcc14eb15e90e6aaf245e01bcd3d1804e452c
Christabel-02 <145669411+Christabel-02@users.noreply.github.com>
2025-05-06 21:15:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Trip/', // 👈 This line tells Vite where the app is hosted
   plugins: [react()],
   server: {
     open: true,
